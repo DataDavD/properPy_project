012118840b33e437ae1c2b5466be5bbb1f776ba8
David Dansby <ddansby89@gmail.com>
2020-06-10 04:10:35 -0700

    Rename tests and add test for testing list args for add func

--- tests/test_sandbox_pytest/test_math_ops.py
+++ tests/test_sandbox_pytest/test_math_ops.py
@@ -7,17 +7,26 @@ def test_ints():
     return 1, 2, 3, 4
 
 
-def test_add(test_ints) -> None:
+@pytest.fixture()
+def test_list():
+    return [1, 2, 3, 4]
+
+
+def test_add_tuple(test_ints) -> None:
     assert add(test_ints) == 10
 
 
-def test_sub(test_ints) -> None:
+def test_add_list(test_list) -> None:
+    assert add(test_list) == 10
+
+
+def test_sub_tuple(test_ints) -> None:
     assert sub(test_ints) == -8
 
 
-def test_multiply(test_ints) -> None:
+def test_multiply_tuple(test_ints) -> None:
     assert multiply(test_ints) == 24
 
 
-def test_div() -> None:
+def test_div_tuple() -> None:
     assert div((4, 2, 1)) == 2
