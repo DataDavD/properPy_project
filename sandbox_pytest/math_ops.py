8d037fbc9052c4c0bbff353c1e2cd7f0679585a9
David Dansby <ddansby89@gmail.com>
2020-06-15 02:35:50 -0700

    Add mypy ignore stmts until figure out issue with type hints with functools.reduce and lambda

--- sandbox_pytest/math_ops.py
+++ sandbox_pytest/math_ops.py
@@ -4,35 +4,35 @@ from typing import Sequence, Union
 
 def add(*args: Union[Sequence[int], int]) -> int:
     if all(isinstance(x, int) for x in args):
-        result = reduce((lambda x, y: x + y), args)
-        return result
+        result = reduce((lambda x, y: x + y), args)  # type: ignore
+        return result  # type: ignore
     else:
-        result = reduce((lambda x, y: x + y), *args)
-        return result
+        result = reduce((lambda x, y: x + y), *args)  # type: ignore
+        return result  # type: ignore
 
 
 def sub(*args: Union[Sequence[int], int]) -> int:
     if all(isinstance(x, int) for x in args):
-        result = reduce((lambda x, y: x - y), args)
-        return result
+        result = reduce((lambda x, y: x - y), args)  # type: ignore
+        return result  # type: ignore
     else:
-        result = reduce((lambda x, y: x - y), *args)
-        return result
+        result = reduce((lambda x, y: x - y), *args)  # type: ignore
+        return result  # type: ignore
 
 
 def multiply(*args: Union[Sequence[int], int]) -> int:
     if all(isinstance(x, int) for x in args):
-        result = reduce((lambda x, y: x * y), args)
-        return result
+        result = reduce((lambda x, y: x * y), args)  # type: ignore
+        return result  # type: ignore
     else:
-        result = reduce((lambda x, y: x * y), *args)
-        return result
+        result = reduce((lambda x, y: x * y), *args)  # type: ignore
+        return result  # type: ignore
 
 
 def div(*args: Union[Sequence[int], int]) -> int:
     if all(isinstance(x, int) for x in args):
-        result = reduce((lambda x, y: x / y), args)
-        return result
+        result = reduce((lambda x, y: x / y), args)  # type: ignore
+        return result  # type: ignore
     else:
-        result = reduce((lambda x, y: x / y), *args)
-        return result
+        result = reduce((lambda x, y: x / y), *args)  # type: ignore
+        return result  # type: ignore
